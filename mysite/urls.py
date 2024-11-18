1f4c1f3f16f167857f3b2b917faaedb4c5d02a16
SSASSHHAA <150457776+SSASSHHAA@users.noreply.github.com>
2024-11-18 20:43:06 +0300

    to

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path("users/", include("users.urls", namespace="users")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
