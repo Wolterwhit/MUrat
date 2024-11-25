6262230027a690cf16fdc9d9b89f06e5857178e2
SSASSHHAA <150457776+SSASSHHAA@users.noreply.github.com>
2024-11-25 19:14:52 +0300

    tookko

--- users/models.py
+++ users/models.py
@@ -2,11 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(blank=True, upload_to='_profile_images')
-    contact_number = models.CharField(max_length=50, default="+7928123456789")
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     image = models.ImageField(blank=True, upload_to='_profile_images')
+#     contact_number = models.CharField(max_length=50, default="+7928123456789")
 
-    def __str__(self):
-        return self.user.username
+#     def __str__(self):
+#         return self.user.username
     
+
+
+class Profile(models.Model): 
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    image = models.ImageField(blank=True, upload_to='_profile_images') 
+    contact_number = models.CharField(max_length=50, blank=False)  # Обязательное поле
+ 
+    def str(self): 
+        return self.user.username    
